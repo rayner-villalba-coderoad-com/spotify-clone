829cdabe6a3c9cce37a8c2146837c5ccea9550a2
rayner-villalba-coderoad-com <rayner.villalba@coderoad.com>
2022-01-03 18:07:43 -0400

    Adding Player Layout

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,10 +1,17 @@
 import {Box} from '@chakra-ui/layout';
 
-const PlayerLayout = ({children}) => {
+const PlayerLayout = ({ children }) => {
   return (
-    <Box>
-      PLayer Layout
-      {children}
+    <Box width='100vw' height='100vh'>
+      <Box position='absolute' top='0' width='250px' left='0'>
+        SideBar
+      </Box>
+      <Box marginLeft='250px' marginBottom='100px'>
+        {children}
+      </Box>
+      <Box position='absolute' left='0' bottom='0'>
+        Player
+      </Box>
     </Box>
   );
 };
