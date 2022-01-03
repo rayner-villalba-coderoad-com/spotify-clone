1aef2d4832be13a664b39e517384619e0873f53e
rayner-villalba-coderoad-com <rayner.villalba@coderoad.com>
2022-01-03 17:42:57 -0400

    My first Layout

---
+++ .history/pages/_app_20220103170912.tsx
@@ -0,0 +1,8 @@
+import {ChakraProvider, extendTheme} from '@chakra-ui/react';
+import 'reset-css';
+
+const  MyApp = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
