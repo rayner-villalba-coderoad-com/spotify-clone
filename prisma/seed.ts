ef1b3b197ced6442a0bbb8b187cd73892cfb2d97
rayner-villalba-coderoad-com <rayner.villalba@coderoad.com>
2022-01-18 14:35:06 -0400

    Adding song stuff and second db migration

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -15,12 +15,40 @@ const run = async () => {
           create: artist.songs.map(song => ({
             name: song.name,
             duration: song.duration,
+            url: song.url,
           })
           )
         }
       }
     })
-  }))
+  }));
+
+  const salt = bcrypt.genSaltSync();
+  const user = await prisma.user.upsert({
+    where: {email: 'user@test.com'},
+    update: {},
+    create: {
+      email: 'user@test.com',
+      password: bcrypt.hashSync('password', salt),   
+    },
+  });
+
+  const songs = await prisma.song.findMany({});
+  await Promise.all(new Array(10).fill(1).map(async(_, i) => {
+    return prisma.playlist.create({
+      data: {
+        name: `Playlist #${i + 1}`,
+        user: {
+          connect: { id: user.id },
+        },
+        songs: {
+          connect: songs.map(song => ({
+            id: song.id,
+          })),
+        },
+      },
+    })
+  }));
 };
 
 run()
