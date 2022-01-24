a07d453a4c99bf44de6838c4e831b90f0a100c19
rayner-villalba-coderoad-com <rayner.villalba@coderoad.com>
2022-01-24 17:34:26 -0400

    Adding submit form

--- components/authForm.tsx
+++ components/authForm.tsx
@@ -4,21 +4,43 @@ import { useSWRConfig } from 'swr';
 
 import { auth } from '../lib/mutations';
 import { FC, useState } from 'react';
+import NextImage from 'next/image';
 
-const AuthForm: FC<{mode: string}> = ({mode}) => {
+const AuthForm: FC<{mode: 'signin' | 'signup'}> = ({mode}) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   const router = useRouter();
   
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    await auth(mode, { email, password });
+    setIsLoading(false);
+    router.push('/');
+  };
+
   return (
     <Box height='100vh' width='100vw' bg='black' color='white'>
-      <Flex justify='center' align='center' height='100px'>
-        hello
+      <Flex justify='center' align='center' height='100px' borderBottom='white 1px solid'>
+       <NextImage src='/logo.svg' height={60} width={120}  />
       </Flex>
       <Flex justify='center' align='center' height='calc(100vh - 100px)'>
-        form
+        <Box padding='50px' bg='gray.900' borderRadius='6px'>
+          <form onSubmit={handleSubmit}>
+            <Input placeholder='Email' type='email' onChange={(e) => setEmail(e.target.value)} />
+            <Input placeholder='Password' type='password' onChange={(e) => setPassword(e.target.value)} />
+            <Button type='submit' bg='green.500' isLoading={isLoading} sx={{
+              '&:hover': {
+                bg: 'green.400'
+              }
+            }}>
+              {mode}
+            </Button>
+          </form>
+        </Box>
       </Flex>
     </Box>
   );
