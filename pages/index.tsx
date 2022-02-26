c4fae8129f052b2cf9caf860bffeaaeb6c317c45
rayner-villalba-coderoad-com <rayner.villalba@coderoad.com>
2022-02-26 18:53:05 -0400

    Adding home user data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,54 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Image } from '@chakra-ui/react'
 import GradientLayout from '../components/gradientLayout';
+import prisma from '../lib/prisma';
+import { Box, Text, Flex } from '@chakra-ui/layout';
+import { useMe } from '../lib/hooks';
 
-const Home = () => {
+const Home = ({ artists }) => {
+  const { user } = useMe();
+
   return (
     <GradientLayout 
       roundImage
       color='green'
       subTitle='profile'
-      title='Rayner Villalba'
-      description='15 public Playlists'
+      title={`${user?.firstName} ${user?.lastName}`}
+      description={`${user?.playlistsCount} public playlists`}
       image='https://tinted-gym-f99.notion.site/image/https%3A%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png%3Fdl%3D0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2'>
-      <div>Holaaa Component</div>
+      <Box color='white' paddingX='40px'>
+        <Box marginBottom='40px'>
+          <Text fontSize='2xl' fontWeight='bold'>
+            Top artist this month
+          </Text>
+          <Text fontSize='md'>only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX="10px" width="20%">
+              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
+                <Image
+                  src="https://placekitten.com/300/300"
+                  borderRadius="100%"
+                />
+                <Box marginTop="20px">
+                  <Text fontSize="large">{artist.name}</Text>
+                  <Text fontSize="x-small">Artist</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   )
+};
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({});
+
+  return {
+    props: { artists },
+  };
 };
 
 export default Home;
